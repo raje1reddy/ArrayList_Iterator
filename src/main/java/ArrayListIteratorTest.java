7bac5543dcebffe183b182153ddd8133e4239fe9
raje reddy <raje1reddy@gmail.com>
2019-09-09 13:19:19 +0100

    Array list using list iterator

--- src/main/java/ArrayListIteratorTest.java
+++ src/main/java/ArrayListIteratorTest.java
@@ -1,16 +1,21 @@
 
+import java.util.ListIterator;
+import java.util.ListResourceBundle;
 import java.util.Scanner;
 import java.util.ArrayList;
-//code accesses and prints all the elements of array list
-public class PadmajaArrayTest {
+//code for how to use a ListIterator to loop through all the values of an  array list
+public class ArrayListIteratorTest {
     public static void main(String[] args){
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("one");
-        list.add("two");
-        list.add("four");
-        list.add(2, "three");
-        for(String element : list){
-            System.out.println(element);
+        ArrayList<String> myArrlist = new ArrayList<String>();
+        myArrlist.add("one");
+        myArrlist.add("two");
+        myArrlist.add("four");
+        myArrlist.add(2, "three");
+        //Get the iterator
+        ListIterator<String> iterator = myArrlist.listIterator();
+        //Use hasNext() to check whether more elements exist
+        while (iterator.hasNext()){
+            System.out.println(iterator.next()); //call next() to get the next item from itwrator
         }
 
     }
